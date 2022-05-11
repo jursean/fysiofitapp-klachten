a179a9795983dcb5108b3b453a1c35711a8aeea8
Duncan de Boer <duncan.d.boer@gmail.com>
2022-05-11 20:48:28 +0200

    applied base styling

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text style={styles.text}>
+                Open up App.js to start working on your app!
+            </Text>
             <StatusBar style="auto" />
         </View>
     );
@@ -13,8 +15,9 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#1F6AB1',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    text: { color: '#ffffff' },
 });
